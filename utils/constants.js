17ad20c6bbbfb31be37b138212eee0c402631451
Liam Devine <skynotebook@proton.me>
2025-06-19 18:42:40 -0400

    Changed status codes to constants

--- utils/constants.js
+++ utils/constants.js
@@ -5,13 +5,17 @@ const DEFAULT = 500;
 const errorHandler = (err, req, res, next) => {
   // Log the exception
   if (err.name === "DocumentNotFoundError") {
-    res.status(NOT_FOUND).send({ status: 404, message: err.message });
+    res.status(NOT_FOUND).send({ status: NOT_FOUND, message: err.message });
   } else if (err.name === "CastError") {
-    res.status(BAD_REQUEST).send({ status: 400, message: "Invalid parameter" });
+    res
+      .status(BAD_REQUEST)
+      .send({ status: BAD_REQUEST, message: "Invalid parameter" });
   } else if (err.name === "ValidationError") {
-    res.status(BAD_REQUEST).send({ status: 400, message: "Invalid data" });
+    res
+      .status(BAD_REQUEST)
+      .send({ status: BAD_REQUEST, message: "Invalid data" });
   } else {
-    res.status(DEFAULT).send({ status: 500, message: err.message });
+    res.status(DEFAULT).send({ status: DEFAULT, message: err.message });
   }
   next();
 };
